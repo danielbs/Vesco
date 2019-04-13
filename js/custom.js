3cc85cb46ef561d14c1e38142e6ab49789c115ce
Daniel <daniel@benshabtay.com>
2019-04-13 23:29:35 +0300

    working on team

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,28 @@ $(function() {
     //animate on scroll
     new WOW().init();
 });
+
+/*====================================================
+                WORK
+====================================================*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    })
+});
+
+/*====================================================
+                TEAM
+====================================================*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true
+    });
+});
